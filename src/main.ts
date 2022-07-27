28d0dd486627f15c74b67441a8b2a51b98054f98
Mohammad Twin <scientists.twin@gmail.com>
2022-07-27 18:40:25 +0400

    feat: swagger was added to project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
@@ -7,6 +8,14 @@ async function bootstrap() {
   const PORT = 3001;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('DEselect Test')
+    .setDescription('The Student API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT, () => {
     Logger.verbose(`Server is running on PORT ${PORT}`, 'NestApplication');
