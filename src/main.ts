ac9585891cd51e4023e537ab8496425298fc8074
Mohammad Twin <scientists.twin@gmail.com>
2022-07-27 17:47:36 +0400

    feat: mongo and mongoose initialled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 3001;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  await app.listen(PORT, () => {
+    Logger.verbose(`Server is running on PORT ${PORT}`, 'NestApplication');
+  });
 }
 bootstrap();
