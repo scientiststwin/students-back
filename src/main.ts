3c4857ef6bccfdc75463d75e23386008f56a92d3
Mohammad Twin <scientists.twin@gmail.com>
2022-07-27 18:30:18 +0400

    feat: two packages (class-validator, class-transaform) was added to project for validate input
    feat: global seeting was added for valdition input

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = 3001;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () => {
     Logger.verbose(`Server is running on PORT ${PORT}`, 'NestApplication');
