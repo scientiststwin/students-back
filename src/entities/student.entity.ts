402da0a413cae9ac0dbbebdf605c8cae35c1d524
Mohammad Twin <scientists.twin@gmail.com>
2022-07-27 18:25:15 +0400

    feat: a entity was added for students

---
+++ src/entities/student.entity.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type StudentDocument = Student & Document;
+
+@Schema({ timestamps: true })
+export class Student {
+  @Prop({ type: Number, required: true, default: null })
+  id: string;
+
+  @Prop({ type: Number, required: false, default: null })
+  age: string;
+
+  @Prop({ type: String, required: false, default: null })
+  firstName: string;
+
+  @Prop({ type: String, required: false, default: null })
+  lastName: string;
+
+  @Prop({ type: String, required: false, default: null })
+  nationality: string;
+}
+
+export const StudentSchema = SchemaFactory.createForClass(Student);
